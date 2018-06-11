bee841e62248a59a1acbc2824b37f60e4fa3404a
xiaoqicheng <1261964818@qq.com>
2018-06-11 18:03:34 +0800

    crontab task test

--- app/Http/Controllers/Console/TestController.php
+++ app/Http/Controllers/Console/TestController.php
@@ -7,6 +7,14 @@ use App\Http\Controllers\Controller;
 class TestController extends Controller{
 
     public function test1(){
-        echo date('Y-m-d H:i:s').'每分钟执行一次';
+        echo date('Y-m-d H:i:s').'  每分钟执行一次';
+    }
+
+    public function test2(){
+        echo date('Y-m-d H:i:s') . '  每小时执行一次';
+    }
+
+    public function test3(){
+        echo date('Y-m-d H:i:s') . '  凌晨执行一次';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,13 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::group(['prefix' => 'console','namespace' => 'Console'],function ($router)
+{
+    $router->get('test1', 'TestController@test1')->name('console.test1');
+    $router->get('test2', 'TestController@test2')->name('console.test2');
+    $router->get('test3', 'TestController@test3')->name('console.test3');
+});
+
 /*Route::group(['prefix' => 'admin','namespace' => 'Admin'],function ($router)
 {
     $router->get('login', 'LoginController@showLoginForm')->name('admin.login');
